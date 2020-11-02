b62aa884d76ea162f4eb4803c75c86c052371fd9
Wesley Vercoutere <wesley.vercoutere@gmail.com>
2020-11-02 22:13:01 +0100

    update

--- app/DummyGPIO.py
+++ app/DummyGPIO.py
@@ -3,26 +3,94 @@ from tkinter import *
 class DummyGPIO():
 
     def __init__(self, tkinterApp):
+        print("Project doesn't run on a Raspberry Pi.\nSimulation will start!\n")
+
         self.tkinterApp = tkinterApp
+        self.BCM = "bcm"
+        self.OUT= "out"
+        self.IN = "in"
+        self.HIGH = "high"
+        self.LOW = "low"
+        self.PUD_DOWN = "Pull down"
+
+        self.inputs = []
+        self.outputs = []
+
+    def setwarnings(self, warning):
+        print(f"Set warnings -> {warning}\n")
+
+        if self.tkinterApp:
+            self.setTopLevel()
+        else:
+            self.setRoot()
+
+
+    def setmode(self, mode):
+        print(f"Set mode -> {mode}\n")
+
+
+    def setup(self, pin, typeInOut, pull_up_down=""):
+        print(f"setup = {pin} {typeInOut} {pull_up_down}")
+
+        if typeInOut == "in":
+            self.addOutput(DummyInput(pin))
+
+        if typeInOut == "out":
+            self.addInput(DummyOutput(pin))
+
+
+    def output(self, pin, output):
+        print(f"output pin number {pin} {output}")
+
+
+    def input(self, pin):
+        print(f"input pin number {pin}")
+
 
     def setTopLevel(self):
-        dummyroot = Toplevel()
-        dummyroot.title("Raspberry Pi dummy GPIO")
-        dummyroot.geometry("400x200")
+        self.dummyroot = Toplevel()
+        self.addFrames()
+
 
     def setRoot(self):
-        pass
+        self.dummyroot = Tk()
+        self.addFrames()
 
 
-    
+    def addFrames(self):
+        self.dummyroot.title("Raspberry Pi dummy GPIO")
+        # self.dummyroot.geometry("400x200")
 
-    # frameInput = LabelFrame(dummyroot, text="Inputs")
-    # frameOutput = LabelFrame(dummyroot, text="Outputs")
+        self.frameInput = LabelFrame(self.dummyroot, text="Inputs")
+        self.frameOutput = LabelFrame(self.dummyroot, text="Outputs")
 
-    # frameInput.pack()
-    # frameOutput.pack()
-
-    # test = Label(dummyroot, text="test")
-    # test.pack()
+        self.frameInput.pack()
+        self.frameOutput.pack()
 
 
+    def addInput(self, input):
+        self.inputs.append(input)
+
+        btn = Button(self.frameInput, text=input.pin)
+        btn.pack()
+
+
+    def addOutput(self, output):
+        self.outputs.append(output)
+
+        lbl = Label(self.frameOutput, text=output.pin)
+        lbl.pack()
+
+
+class DummyOutput():
+
+    def __init__(self, pin):
+        self.pin = pin
+        self.state = False
+
+
+class DummyInput():
+
+    def __init__(self, pin):
+        self.pin = pin
+        self.state = False
--- app/MainDummyGPIO.py
+++ app/MainDummyGPIO.py
@@ -11,6 +11,39 @@ root = Tk()
 root.title("Test proggie dummy GPIO")
 root.geometry("800x400")
 
+btnBlue = Button(root, text="Blue led")
+btnRed = Button(root, text="Red led")
+btnYellow = Button(root, text="Yellow led")
+btnGreen = Button(root, text="Green led")
+
+btnBlue.place(x=10,y=10)
+btnRed.place(x=10,y=60)
+btnYellow.place(x=10,y=110)
+btnGreen.place(x=10,y=160)
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+ledBlue = 19
+ledRed = 20
+ledYellow = 21
+ledGreen = 22
+
+button1 = 23
+button2 = 24
+button3 = 25
+button4 = 26
+
+leds = (ledBlue, ledRed, ledYellow, ledGreen)
+buttons = (button1, button2, button3, button4)
+
+for led in leds:
+    GPIO.setup(led, GPIO.OUT)
+
+for btn in buttons:
+    GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 
 root.mainloop()

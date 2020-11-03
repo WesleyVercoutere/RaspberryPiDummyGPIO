a0ef35c00a3a80f54cfd958ce5cf1e14de248134
WesleyVercoutere <wesley.vercoutere@gmail.com>
2020-11-03 09:31:56 +0100

    update

--- app/DummyGPIO.py
+++ app/DummyGPIO.py
@@ -16,6 +16,12 @@ class DummyGPIO():
         self.inputs = []
         self.outputs = []
 
+        self.rowInput = 0
+        self.columnInput = 0
+        self.rowOutput = 0
+        self.columnOutput = 0
+
+
     def setwarnings(self, warning):
         print(f"Set warnings -> {warning}\n")
 
@@ -55,31 +61,35 @@ class DummyGPIO():
     def setRoot(self):
         self.dummyroot = Tk()
         self.addFrames()
+        self.dummyroot.mainloop()
 
 
     def addFrames(self):
         self.dummyroot.title("Raspberry Pi dummy GPIO")
-        # self.dummyroot.geometry("400x200")
 
         self.frameInput = LabelFrame(self.dummyroot, text="Inputs")
         self.frameOutput = LabelFrame(self.dummyroot, text="Outputs")
 
-        self.frameInput.pack()
-        self.frameOutput.pack()
+        self.frameInput.grid(row=0, column=0, padx=10, pady=10)
+        self.frameOutput.grid(row=0, column=1, padx=10, pady=10)
 
 
     def addInput(self, input):
         self.inputs.append(input)
 
-        btn = Button(self.frameInput, text=input.pin)
-        btn.pack()
+        btn = Button(self.frameInput, text=input.pin, padx=10, pady=10)
+        btn.grid(row=self.rowInput, column=self.columnInput, padx=10, pady=10)
+
+        self.rowInput += 1
 
 
     def addOutput(self, output):
         self.outputs.append(output)
 
-        lbl = Label(self.frameOutput, text=output.pin)
-        lbl.pack()
+        lbl = Label(self.frameOutput, text=output.pin, padx=10, pady=10)
+        lbl.grid(row=self.rowOutput, column=self.columnOutput, padx=10, pady=10)
+
+        self.rowOutput += 1
 
 
 class DummyOutput():
